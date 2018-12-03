60e9bd8dfaebb86ac41e12bc07a04b252896987a
Joel Keyser <keyserj@uwplatt.edu>
2018-12-02 21:52:38 -0600

    Add initial work.
    Splits PDFs by specified page number. Tests for this do not yet cleanup the created files, nor do they actually compare that the created and copied files are equal.

---
+++ PdfSplitter/PdfSplitter.cs
@@ -0,0 +1,76 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Path = System.IO.Path;
+
+namespace PdfSplitter
+{
+    public class PdfSplitter
+    {
+        public readonly string InputFilePath;
+        public readonly string OutputDirectory;
+
+        public PdfSplitter(string inputFilePath, string outputPath)
+        {
+            InputFilePath = inputFilePath;
+            OutputDirectory = outputPath;
+        }
+
+        public List<string> SplitByKeyword(string keyword)
+        {
+            throw new NotImplementedException();
+            //List<int> pagesWithKeyWord = GetPagesWithKeyword(keyword);
+            //SplitPdfsByPages(pagesWithKeyWord);
+        }
+
+        public List<string> SplitByPages(List<int> pagesToSplit)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<int> GetPagesWithKeyword(string keyword)
+        {
+            List<int> pagesWithKeyword = new List<int>();
+
+            using (PdfReader pdfReader = new PdfReader(InputFilePath))
+            {
+                for (int page = 1; page <= pdfReader.NumberOfPages; page++)
+                {
+                    string pageText = PdfTextExtractor.GetTextFromPage(pdfReader, page);
+                    if (pageText.Contains(keyword))
+                    {
+                        pagesWithKeyword.Add(page);
+                    }
+                }
+            }
+
+            return pagesWithKeyword;
+        }
+
+        public string CreateCopyForRange(int startPage, int endPage)
+        {
+            string inputFileName = Path.GetFileNameWithoutExtension(InputFilePath);
+            string outputFileName = $"{inputFileName}_pg_{startPage}_thru_{endPage}.pdf";
+            string outputFilePath = Path.Combine(OutputDirectory, outputFileName);
+            Directory.CreateDirectory(OutputDirectory);
+
+            using (PdfReader pdfReader = new PdfReader(InputFilePath))
+            using (Document sourceDocument = new Document(pdfReader.GetPageSizeWithRotation(startPage)))
+            using (FileStream fileStream = new FileStream(outputFilePath, FileMode.Create))
+            using (PdfCopy pdfCopy = new PdfCopy(sourceDocument, fileStream))
+            {
+                sourceDocument.Open();
+                for (int page = startPage; page <= endPage; page++)
+                {
+                    PdfImportedPage pdfImportedPage = pdfCopy.GetImportedPage(pdfReader, page);
+                    pdfCopy.AddPage(pdfImportedPage);
+                }
+                sourceDocument.Close();
+            }
+            return outputFilePath;
+        }
+    }
+}
---
+++ PdfSplitterTests/PdfSplitterTests.cs
@@ -0,0 +1,101 @@
+﻿using iTextSharp.testutils;
+using PdfSplitter;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Xunit;
+
+namespace PdfSplitterTests
+{
+    public class PdfSplitterTests
+    {
+        private readonly PdfSplitter.PdfSplitter _pdfSplitter;
+
+        public PdfSplitterTests()
+        {
+            string inputFilePath = Resources.PdfTestPath;
+            string outputDirectory = Resources.OutputDirectory;
+            _pdfSplitter = new PdfSplitter.PdfSplitter(inputFilePath, outputDirectory);
+        }
+
+        [Fact]
+        public void Get_page_with_keyword()
+        {
+            string keyword = "Adobe PDF Library customers";
+
+            List<int> pagesWithKeyword = _pdfSplitter.GetPagesWithKeyword(keyword);
+
+            Assert.Contains(2, pagesWithKeyword);
+            Assert.Contains(5, pagesWithKeyword);
+            Assert.Contains(6, pagesWithKeyword);
+            Assert.DoesNotContain(1, pagesWithKeyword);
+            Assert.DoesNotContain(3, pagesWithKeyword);
+            Assert.DoesNotContain(4, pagesWithKeyword);
+        }
+
+        //[Fact]
+        //public void Split_by_keyword()
+        //{
+        //    string keyword = "Adobe PDF Library customers";
+
+        //    try
+        //    {
+        //        List<string> pathsToCreatedPdfs = _pdfSplitter.SplitByKeyword(keyword);
+        //    }
+        //    finally
+        //    {
+        //        // cleanup
+        //    }
+        //}
+
+        //[Fact]
+        //public void Split_by_pages()
+        //{
+        //    List<int> pagesToSplit = new List<int>() { 2, 5, 6 };
+
+        //    try
+        //    {
+        //        List<string> pathsToCreatedPdfs = _pdfSplitter.SplitByPages(pagesToSplit);
+        //    }
+        //    finally
+        //    {
+        //        // cleanup
+        //    }
+        //}
+
+        [Theory]
+        [InlineData(1, 2)]
+        [InlineData(3, 5)]
+        public void Create_copy_for_range(int startPage, int endPage)
+        {
+            string pathToCreatedPdf = _pdfSplitter.CreateCopyForRange(startPage, endPage);
+
+            //try
+            //{
+            using (FileStream _ = File.OpenRead(pathToCreatedPdf))
+            {
+
+            }
+
+            CompareTool compareTool = new CompareTool();
+            string comparison =
+                compareTool.Compare(
+                    pathToCreatedPdf,
+                    _pdfSplitter.InputFilePath,
+                    Resources.OutputDirectory,
+                    "diff");
+            Assert.Null(comparison);
+            //AssertPdfCreatedForPages(pathToCreatedPdf, startPage, endPage);
+            //}
+            //finally
+            //{
+            //    Cleanup(pathToCreatedPdf);
+            //}
+        }
+
+        private void AssertPdfCreatedForPages(string pathToCreatedPdf, int startPage, int endPage)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
